a15e64193397ee4e49be9752549dc129c5e15e5f
Prakash Raut <prakash_raut@outlook.com>
2024-10-07 16:21:19 +0530

    added producer app

---
+++ apps/producer/src/index.ts
@@ -0,0 +1,42 @@
+import { Kafka } from "kafkajs";
+import {db} from "@repo/db";
+
+const TOPIC_NAME = "zap-events";
+
+const kafka = new Kafka({
+	clientId: "outbox-processor",
+	brokers: ["localhost:9092"],
+});
+
+async function main() {	
+	const producer = kafka.producer();
+	await producer.connect();
+
+	while (1) {
+		const pendingRows = await db.zapRunOutbox.findMany({
+			where: {},
+			take: 10,
+		});
+
+		producer.send({
+			topic: TOPIC_NAME,
+			messages: pendingRows.map((r) => {
+				return {
+					value: JSON.stringify({ zapRunId: r.zapRunId, stage: 0 }),
+				};
+			}),
+		});
+
+		await db.zapRunOutbox.deleteMany({
+			where: {
+				id: {
+					in: pendingRows.map((x) => x.id),
+				},
+			},
+		});
+
+		await new Promise((r) => setTimeout(r, 3000));
+	}
+}
+
+main();
