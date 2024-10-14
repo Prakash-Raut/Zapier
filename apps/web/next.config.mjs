649957d0347fbe53703684f80ab30ca5191ecd09
Prakash Raut <prakash_raut@outlook.com>
2024-10-14 19:57:43 +0530

    chore: update next.config

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 import dotenv from 'dotenv';
-import {resolve} from 'path';
+import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config({path: resolve(__dirname, '../../env')});
+dotenv.config({path: path.resolve(__dirname, '../env')});
 
 const nextConfig = {
     env: {
